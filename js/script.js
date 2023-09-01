bc723dfcdc69cf587de2e3d27b18210e2f62faa2
mdshakhawat-H <mdshakhawathossain441@hotmail.com>
2023-09-01 05:56:22 -0600

    REsponsive Edit

--- js/script.js
+++ js/script.js
@@ -1,81 +1,3 @@
-/*
-// When we have a constructor functon we can assign properties using the this keyword
-const TypeWriter = function(txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
-}
-
-// Type Method
-TypeWriter.prototype.type = function() {
-    // console.log('Hello');
-    
-    // Current index of word
-    // We use the modulus operator to get the total length of the words
-    const current = this.wordIndex % this.words.length;
-    // console.log(current);
-    
-    // Get full text of current word
-    const fullTxt = this.words[current];
-    // console.log(fullTxt);
-    
-    // Check to see if it's in the deleting state
-    if(this.isDeleting) {
-       // Remove a character
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-       } else {
-       // Add a character
-           this.txt = fullTxt.substring(0, this.txt.length + 1);
-       }
-    
-    // Every half a second output what is in the .txt
-    // Insert txt into element
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-    
-    // Initial Type Speed
-    let typeSpeed = 300;
-    if(this.isDeleting) {
-       typeSpeed /= 2;
-       }
-    // Check to see if the word is complete
-    if(!this.isDeleting && this.txt === fullTxt) {
-        // Make pause at end
-       typeSpeed = this.wait;
-        // Set isDeleting to true
-        this.isDeleting = true;
-       } else if(this.isDeleting && this.txt === '') {
-        // Set isDeleting back to false
-           this.isDeleting = false;
-        // Move to the next word
-           this.wordIndex++;
-        // Pause before start typing again
-           typeSpeed = 500;
-       }
-    
-    
-    // Run it at a certain pace
-    setTimeout(() => this.type(), typeSpeed);
-}
-
-// Initialize on DOM Load
-document.addEventListener('DOMContentLoaded', init);
-
-// Initialize the app in the init function
-function init() {
-    const txtElement = document.querySelector('.txt-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait')
-    // Initialize the TypeWriter
-    new TypeWriter(txtElement, words, wait);
-}
-*/
-
-
-
 
 // ES6 Class
 // constructor is a method that runs when the object is instantiated from the class
@@ -169,7 +91,7 @@ $('.OC_slider').slick({
         breakpoint: 1199.98,
         settings: {
           slidesToShow: 4,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -178,7 +100,7 @@ $('.OC_slider').slick({
         breakpoint: 991.98,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -187,7 +109,7 @@ $('.OC_slider').slick({
         breakpoint: 767.98,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
